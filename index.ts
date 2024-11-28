2b0e9b71a1e34b713250132666395708661d777c
JeganMurugan <jegan.murugan@concertidc.com>
2024-11-28 22:37:41 +0530

    update

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { AppDataSource } from "./src/data-source";
 import userRoutes from "./src/routes/userRoutes";
+import cors from "cors"; // Import the CORS package
+
 
 const app = express();
 const PORT = 3000;
+app.use(cors());
 
 app.use(express.json());
 app.get("/", (req, res) => {
