4f2a9203a034868d41bc4aca288a6e751dea8324
JeganMurugan <jegan.murugan@concertidc.com>
2024-11-29 20:15:49 +0530

    update SSL

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
 import express from "express";
+const fs = require('fs');
 import { AppDataSource } from "./src/data-source";
 import userRoutes from "./src/routes/userRoutes";
+const https = require('https');
 import cors from "cors"; // Import the CORS package
 
 const app = express();
-const PORT = 3000;
 app.use(cors());
 
 app.use(express.json());
+
+const privateKey = fs.readFileSync('/etc/ssl/private/server.key', 'utf8');
+const certificate = fs.readFileSync('/etc/ssl/certs/server.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
 app.get("/", (req, res) => {
   res.send("Welcome to the Home Page API!");
 });
@@ -17,8 +23,8 @@ app.use("/users", userRoutes);
 AppDataSource.initialize()
   .then(() => {
     console.log("Data Source has been initialized!");
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
+    https.createServer(credentials, app).listen(443, () => {
+      console.log('Server running on https://localhost:443');
     });
   })
   .catch((error) => {
