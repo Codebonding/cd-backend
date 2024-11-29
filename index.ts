c885badee78b0bcb33db75855a2f9e55c605df2f
Codebonding <codebonding@gmail.com>
2024-11-29 22:32:24 +0530

    fs

--- index.ts
+++ index.ts
@@ -1,22 +1,13 @@
 import express from "express";
 import { AppDataSource } from "./src/data-source";
 import userRoutes from "./src/routes/userRoutes";
-import cors from "cors";
+import cors from "cors"; // Import the CORS package
 
 const app = express();
 const PORT = 3000;
-
-app.use(
-  cors({
-    origin: "https://codebondingworkforce.com", 
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
-
-
 app.get("/", (req, res) => {
   res.send("Welcome to the Home Page API!");
 });
@@ -25,7 +16,7 @@ app.use("/users", userRoutes);
 AppDataSource.initialize()
   .then(() => {
     console.log("Data Source has been initialized!");
-    app.listen(PORT, "0.0.0.0", () => {
+    app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   })
